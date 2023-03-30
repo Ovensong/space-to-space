969885c942c30fb00cb226867dc7e254ac4dae2a
Ovensong <115195584+Ovensong@users.noreply.github.com>
2023-03-30 06:16:14 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>;
+#include<Windows.h>;
+
+int main() {
+	while (1 == 1) {
+		::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+		if (GetKeyState(' ') & 0x8000)
+		{
+			keybd_event('S', 0, 0, 0);
+			keybd_event('P', 0, 0, 0);
+			keybd_event('A', 0, 0, 0);
+			keybd_event('C', 0, 0, 0);
+			keybd_event('E', 0, 0, 0);
+			Sleep(100);
+		}
+	}
+	return 0;
+}
